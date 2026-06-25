bf6426bb0c11166dfd2dde46fa78d910a90da18e
AR-CA-SA <camiloydios87@gmail.com>
2026-06-25 09:40:46 -0700

    6/25

--- object_segmentation/analysis.py
+++ object_segmentation/analysis.py
@@ -2,3 +2,5 @@ from ultralytics import YOLO
 
 
 model = YOLO("yolo11n.pt")
+shared_frame = SharedMemory(name="SharedFrame", create=False)
+
--- object_segmentation/main_frame.py
+++ object_segmentation/main_frame.py
@@ -1,30 +1,35 @@
 import cv2 
+import sys
+import argparse
 import numpy as np
-import  asyncio
+import asyncio
 import threading
 import time
 import sys
 from multiprocessing import Process
 from multiprocessing.shared_memory import SharedMemory
 
+from ultralytics import YOLO
+
+
+model = YOLO("yolo11n.pt")
+
 
 shared_frame = SharedMemory(name="sharedFrame", size=8,create=True)
 
 
 
-def video_capture(video_url):
-    cap = cv2.VideoCapture(video_url)
-    while cap.isOpened():
-        success, frame = cap.read()
-        if not success:
-            break
-        cv2.imshow("s",frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+def main(video_url):
+    image = np.array([])
+    video = cv2.VideoCapture(video_url)
+    success, frame = video.read()
+    success, frame = video.read()
+    cv2.im
+    while success:
+        video.set
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     video_capture(video_url="test_video.mp4")
+    shared_frame.close()
+    shared_frame.unlink()
