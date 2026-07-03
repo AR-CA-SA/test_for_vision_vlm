a4aca376fe1a55969bb8db969ef8e7e8d7b8ae77
AR-CA-SA <camiloydios87@gmail.com>
2026-07-02 17:25:27 -0700

    ds

--- object_segmentation/gemini_er.py
+++ object_segmentation/gemini_er.py
@@ -1,7 +1,7 @@
 import signal
 import time
 import os
-
+import asyncio
 import multiprocessing
 import cv2
 import threading 
@@ -13,24 +13,28 @@ from multiprocessing.managers import BaseManager
 class QueueManager(BaseManager):
     pass
 
-# def initialize_vlm(path_to_object_image):
-#     client = genai.Client()
-#     uploaded_file = client.files.upload(file = "object_segmentation/for_vlm.jpeg")
-#     interaction  = client.interactions.create(
-#         model = "gemini-3.5-flash", 
-#         input = [
-#             {"type":"text", "text" : "Given the image of the household object, I want you t"} ,
-#             {"type" : "image", "uri" : uploaded_file.uri, "mime_type" : uploaded_file.mime_type}])
-#     return interaction.output_text
+async def initialize_vlm(path_to_object_image):
+    client = genai.Client()
+    uploaded_file = client.files.upload(file = "object_segmentation/for_vlm.jpeg")
+    interaction  = client.interactions.create(
+        model = "gemini-3.5-flash", 
+        input = [
+            {"type":"text", "text" : "Given the image of the household object, I want you t"} ,
+            {"type" : "image", "uri" : uploaded_file.uri, "mime_type" : uploaded_file.mime_type}])
+    print(interaction.output_text)
+    return interaction.output_text
 
-def data_stream_handler():
+async def data_stream_handler():
     QueueManager.register('get_queue')
     manager = QueueManager(address=('', 50000), authkey=b'abc')
     manager.connect()
     remote_queue = manager.get_queue()
     path_to_object_image = remote_queue.get(timeout = 2)
+    await asyncio.create_task(initialize_vlm(path_to_object_image))
     print("Received path to object image from shared queue:", path_to_object_image)
 
+
+    subprocess.run(f"rm -rf frames/{path_to_object_image}", shell=True, check=True)
 if __name__=="__main__":
     data_stream_handler()
 
